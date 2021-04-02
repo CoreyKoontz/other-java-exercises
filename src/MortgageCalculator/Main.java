11b739afe9c81caf4114f45f5b78eaaf66a6c36d
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-02 13:17:45 -0500

    Refactoring mortgage calc: moved user input functionality outside of main method

--- src/MortgageCalculator/Main.java
+++ src/MortgageCalculator/Main.java
@@ -1,71 +1,58 @@
-//package MortgageCalculator;
-//
-//import java.text.NumberFormat;
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//
-//        // Setting some finals to known values to add for readability later.
-//        final byte MONTHS_IN_YEAR = 12;
-//        final byte PERCENT = 100;
-//
-//        // setting an instance of scanner to the sc variable so that
-//        // every time sc is called we get another instance of Scanner.
-//        Scanner sc = new Scanner(System.in);
-//
-//        // Getting user input: ------------------------------------------
-//
-//        int userPrinciple = 0;
-//        while (true) {
-//            System.out.println("Principle ($1k - $1M): ");
-//             userPrinciple = sc.nextInt();
-//            if (userPrinciple >= 1000 && userPrinciple <= 1_000_000)
-//                break;
-//                System.out.println("Please enter a number between 1,000 and 1,000,000");
-//        }
-//
-//
-//        float userAnnualInterestRate;
-//        while (true) {
-//            System.out.println("Annual Interest Rate: ");
-//             userAnnualInterestRate = sc.nextFloat();
-//            if (userAnnualInterestRate >= 0 && userAnnualInterestRate <= 30)
-//                break;
-//            System.out.println("Please enter a number between 0 and 30");
-//        }
-//
-//        int userPeriodInYears;
-//        while (true) {
-//            System.out.println("Period (Years): ");
-//            userPeriodInYears = sc.nextInt();
-//            if (userPeriodInYears >= 1 && userPeriodInYears <= 30)
-//                break;
-//            System.out.println("Please enter a number between 1 and 30");
-//        }
-//
-//        // r = monthly interest rate
-//        float monthlyInterestRate = (userAnnualInterestRate / PERCENT) / MONTHS_IN_YEAR;
-//
-//        // p = principle
-//        int principle = userPrinciple;
-//
-//        // n = number of payments
-//        double n = userPeriodInYears * MONTHS_IN_YEAR;
-//
-//        // Mortgage calculator:
-//        double mortgage = principle * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), n))) / ((Math.pow((1 + monthlyInterestRate), n)) - 1));
-//
-//        // formatting the result of the mortgage calculator:
-//        NumberFormat currency = NumberFormat.getCurrencyInstance();
-//
-//        // Creating a variable to hold the new formatted currency. For readability.
-//        String formattedMortgage = currency.format(mortgage);
-//
-//        // Printing the formatted calculator results:
-//        System.out.println("Mortgage: " + formattedMortgage);
-//
-//    }
-//
-//}
+package MortgageCalculator;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Setting some finals to known values to add for readability later.
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int userPrinciple = 0;
+        float userAnnualInterestRate;
+        int userPeriodInYears;
+
+        // Getting user input: ------------------------------------------
+
+        userPrinciple = (int) readNumber("Principal: ", 1_000, 1_000_000);
+
+        userAnnualInterestRate = (float) readNumber("Annual Interest Rate: ", 1, 30);
+
+        userPeriodInYears = (int) readNumber("Period (Years): ", 1, 30);
+
+        float monthlyInterestRate = (userAnnualInterestRate / PERCENT) / MONTHS_IN_YEAR;  // r = monthly interest rate
+
+        int principle = userPrinciple;  // p = principle
+
+        double n = userPeriodInYears * MONTHS_IN_YEAR; // n = number of payments
+
+        // Mortgage calculator:
+        double mortgage = principle * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), n))) / ((Math.pow((1 + monthlyInterestRate), n)) - 1));
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();// formatting the result of the mortgage calculator:
+
+        // Creating a variable to hold the new formatted currency. For readability.
+        String formattedMortgage = currency.format(mortgage);
+
+        // Printing the formatted calculator results:
+        System.out.println("Mortgage: " + formattedMortgage);
+
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner sc = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = sc.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.printf("Please enter a number between: %d and %d", min, max);
+        }
+        return value;
+    }
+
+}
