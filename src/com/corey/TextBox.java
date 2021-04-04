081c0bc38d99caac50cc0e11caee1188b13761c6
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-03 19:00:59 -0500

    Updating employee class to help simplify main class

---
+++ src/com/corey/TextBox.java
@@ -0,0 +1,15 @@
+package com.corey;
+
+public class TextBox {
+
+    public String text= ""; // Field
+
+    // Setter:
+    public void setText (String text) {
+        this.text = text;
+    }
+
+    public void clear () {
+        text = "";
+    }
+}
