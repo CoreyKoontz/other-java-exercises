5fd0540efeb763be3ec4b85d29e451064a39ef0e
Corey Koontz <corey.michael.koontz@gmail.com>
2021-06-14 17:36:08 -0500

    second step of arrays exercise complete

--- src/com/codewithmosh/Array.java
+++ src/com/codewithmosh/Array.java
@@ -2,13 +2,44 @@ package com.codewithmosh;
 
 public class Array {
 
-    // initializing items array
     private int[] items;
     private int count;
 
-    //Constructor:
     public Array(int length) {
         items = new int[length];
+    }
+
+
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count * 2];
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+
+            items = newItems;
+        }
+        items[count++] = item;
+    }
+
+    public void removeAt(int index) {
+        //validate index
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+
+        //shift items to the left to fill the hole
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+
+        count--;
+
+    }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < count; i++)
+            if (items[i] == item)
+                return i;
+
+        return -1;
     }
 
     public void print() {
--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -14,6 +14,11 @@ public class Main {
 
 
         Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(0);
         numbers.print();
     }
 }
